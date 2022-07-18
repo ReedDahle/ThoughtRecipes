430439b66f0c9d7cdeeceed0fc073bbd959e1a80
rexius923 <rexius923@gmail.com>
2022-07-18 21:35:33 +0000

    ngihtly

--- static/base.js
+++ static/base.js
@@ -1,1 +1,13 @@
 "use strict";
+(() => {
+    let menuButton = $('#menuButton')[0];
+    let menu = $('#menu')[0];
+    menuButton.onclick = function () {
+        if (menu.getAttribute('display') === 'block') {
+            menu.setAttribute('display', 'none');
+        }
+        else {
+            menu.setAttribute('display', 'block');
+        }
+    };
+})();
